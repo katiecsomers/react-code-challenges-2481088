732175d8be4c7b7cefe19ce535574f659da597ce
katiecsomers <kcornallysomers@gmail.com>
2022-11-25 11:43:35 +1300

    ongoing challenges

--- src/01-color-renderer/ColorRenderer.js
+++ src/01-color-renderer/ColorRenderer.js
@@ -1,27 +1,36 @@
-import Color from './Color'
+import Color from "./Color";
 
-const colors = [{
-  hex: '#91A6FF',
-  name: 'Cornflower Blue'
-},
-{
-  hex: '#FF88DC',
-  name: 'Persian Pink'
-},
-{
-  hex: '#80FF72',
-  name: 'Screamin Green'
-},
-{
-  hex: '#FF5154',
-  name: 'Tart Orange'
-}]
+const colors = [
+	{
+		hex: "#91A6FF",
+		name: "Cornflower Blue",
+	},
+	{
+		hex: "#FF88DC",
+		name: "Persian Pink",
+	},
+	{
+		hex: "#80FF72",
+		name: "Screamin Green",
+	},
+	{
+		hex: "#FF5154",
+		name: "Tart Orange",
+	},
+];
 
-export default function ColorRenderer () {
-  return (
-    <h2>
-      Use the Color component to render each
-       item in the colors array on the page!
-    </h2>
-  )
+export default function ColorRenderer() {
+	return (
+		<h2>
+			{colors.map(function (colors, key) {
+				return (
+					<Color
+						hex={colors.hex}
+						name={colors.name}
+						key={key}
+					/>
+				);
+			})}
+		</h2>
+	);
 }
