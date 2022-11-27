0f14a683adc1e9b8a4059b2375537e779456d954
katiecsomers <kcornallysomers@gmail.com>
2022-11-28 00:00:30 +1300

    Update FocusInput.js

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,11 @@
 export default function FocusInput() {
-  return (
-    <div>
-      <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
-    </div>
-  )
+	return (
+		<div>
+			<label htmlFor="focused-input">Focus me on page load!</label>
+			<input
+				name="focused-input"
+				autoFocus
+			></input>
+		</div>
+	);
 }
