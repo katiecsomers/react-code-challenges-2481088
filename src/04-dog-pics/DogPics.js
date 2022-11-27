fdc1a9a7f2f68c27780906994e689a00619b0205
katiecsomers <kcornallysomers@gmail.com>
2022-11-27 21:25:08 +1300

    update challenge

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,29 @@
-export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
-  return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
-    </div>
-  )
+import { useState } from "react";
+import axios from "axios";
+
+export default function DogPics() {
+	const [url, setUrl] = useState(
+		"https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg"
+	);
+
+	function searchDogs() {
+		axios
+			.get(`https://dog.ceo/api/breeds/image/random`)
+
+			.then((data) => {
+				console.log(data);
+				setUrl(data.data.message);
+			})
+			.catch((error) => console.error(error));
+	}
+
+	return (
+		<div className="dog-pics">
+			<img
+				src={url}
+				alt="Dog pics"
+			/>
+			<button onClick={searchDogs}>🐶</button>
+		</div>
+	);
 }
